3f201ff25618af62b729f09165e2e446bd47e6b1
Mahendar R <mahendar20r@gmail.com>
2026-03-13 20:24:31 +0530

    Update ahb_environment.sv

--- env/ahb_environment.sv
+++ env/ahb_environment.sv
@@ -1,37 +1,50 @@
 class ahb_environment;
-   ahb_generator gen;
-   ahb_driver drv;
-   ahb_monitor mon;
+
+   ahb_generator  gen;
+   ahb_driver     drv;
+   ahb_monitor    mon;
    ahb_scoreboard sco;
-  
-   mailbox #(ahb_transaction)mbxgd = new(1);
-   mailbox #(ahb_transaction)mbxms = new();
-   mailbox #(bit[4:0])mbxgm = new();
-   
- virtual ahb_interface vif;
 
-function new(virtual ahb_interface vif);
-   this.vif = vif;
-endfunction
- 
- task build();
-   gen = new(mbxgd,mbxgm);
-   drv = new(mbxgd,vif);
-   mon = new(mbxms,mbxgm,vif);
-   sco = new(mbxms);
-endtask
+   mailbox #(ahb_transaction) mbxgd;
+   mailbox #(ahb_transaction) mbxms;
+   mailbox #(bit [4:0]) mbxgm;
 
-task run();
-drv.reset();
-   fork
-      gen.run();
-      drv.run();
-      mon.run();
-      sco.run();
-   join_none;
-   wait(stop.triggered);
-   #40;
-   $display("-------------------------------------");
-   $finish;
-endtask
+   virtual ahb_interface vif;
+
+   function new(virtual ahb_interface vif);
+      this.vif = vif;
+   endfunction
+
+   function void build();
+      mbxgd = new();
+      mbxms = new();
+      mbxgm = new();
+      gen   = new(mbxgd, mbxgm);
+      drv   = new(mbxgd, vif);
+      mon   = new(mbxms, mbxgm, vif);
+      sco   = new(mbxms);
+   endfunction
+
+   task run();
+      fork
+         drv.reset();
+      join
+
+      fork
+         gen.run();
+         drv.run();
+         mon.run();
+         sco.run();
+      join_none
+
+      @stop;
+      repeat(10) @(posedge vif.clk);
+
+      $display("--------------------------------------");
+      $display("ENVIRONMENT: TEST COMPLETED");
+      $display("--------------------------------------");
+
+      $finish;
+   endtask
+
 endclass
