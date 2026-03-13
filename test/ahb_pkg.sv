b135f9c25d12de6902a0830a2f17fa6513cb46b5
Mahendar R <mahendar20r@gmail.com>
2026-03-13 20:33:50 +0530

    Update ahb_pkg.sv

--- test/ahb_pkg.sv
+++ test/ahb_pkg.sv
@@ -1,17 +1,20 @@
 package ahb_pkg;
-event drvnext,sconext,done,stop;
-bit d_flag = 0;
-bit s_flag = 0;
 
-event stop;
-`include "ahb_transaction.sv"
-`include "ahb_generator.sv"
-`include "ahb_driver.sv"
-`include "ahb_monitor.sv"
-`include "ahb_coverage.sv"
-`include "ahb_scoreboard.sv"
-`include "ahb_environment.sv"
-`include "ahb_test.sv"
+   // Global sync flags and events
+   event stop;
+   bit   d_flag = 0;
+   bit   s_flag = 0;
 
+   // ENV includes (relative paths - EDA compatible)
+   `include "ahb_transaction.sv"
+   `include "ahb_generator.sv"
+   `include "ahb_driver.sv"
+   `include "ahb_monitor.sv"
+   `include "ahb_coverage.sv"
+   `include "ahb_scoreboard.sv"
+   `include "ahb_environment.sv"
+
+   // TEST include
+   `include "ahb_test.sv"
 
 endpackage
