b626e4606dd28da5c7fb020cd0349e7110ae0e30
Mahendar R <mahendar20r@gmail.com>
2026-03-13 20:24:58 +0530

    Refactor ahb_generator class and update run logic

--- env/ahb_generator.sv
+++ env/ahb_generator.sv
@@ -1,16 +1,26 @@
 class ahb_generator;
-   ahb_transaction tr;
 
-   mailbox  #(ahb_transaction)mbxgd;
+   mailbox #(ahb_transaction) mbxgd;
    mailbox #(bit [4:0]) mbxgm;
-   event done,drvnext,sconext;
-   int count=0;
+   ahb_transaction tr;
+   int count = 1;
 
-   function new(mailbox #(ahb_transaction)mbxgd,mailbox #(bit [4:0]) mbxgm);
-     this.mbxgd = mbxgd;
-     this.mbxgm = mbxgm;
+   function new(mailbox #(ahb_transaction) mbxgd,
+                mailbox #(bit [4:0]) mbxgm);
+      this.mbxgd = mbxgd;
+      this.mbxgm = mbxgm;
    endfunction
 
-virtual task run();
-endtask
+   virtual task run();
+      repeat(count)
+      begin
+         tr = new();
+         if(!tr.randomize())
+            $fatal("Randomization failed");
+         mbxgd.put(tr);
+         if(tr.hburst == 3'b001)
+            mbxgm.put(tr.ulen);
+      end
+   endtask
+
 endclass
